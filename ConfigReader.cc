372b007b42f0cd1a217699cb0e2fcebd81892093
Rodolfo Granata <warlock.cc@gmail.com>
2011-11-13 21:27:27 -0300

    * Enhance Config reading: don't use simple maps.

--- ConfigReader.cc
+++ ConfigReader.cc
@@ -3,46 +3,80 @@
 #include "ConfigReader.h"
 
 #define SUBREGS 3
-ConfigReader::ConfigReader(const std::string &path) {
+#define LINEBUF 256
+ConfigReader::ConfigReader(const std::string &path) :
+    sections() {
   regex_t section_expr, value_expr;
   regmatch_t match_info[SUBREGS];
-  char linebuf[256];
   std::ifstream cfgfile;
+  char linebuf[LINEBUF];
 
   if (regcomp(&section_expr, "^\\[(\\w+)\\]$", REG_EXTENDED))
     throw "Failed to compile section regex.";
   if (regcomp(&value_expr, "^ *([^= ]+) *= *(.*) *$", REG_EXTENDED))
     throw "Failed to compile value regex.";
 
-  std::string section(""); // default section for key-values
   cfgfile.open(path.c_str());
+  if (cfgfile.rdstate() != std::ios_base::goodbit)
+    throw "Failed to open config file.";
+
   while (!cfgfile.getline(linebuf, sizeof(linebuf)).eof()) {
+
     if (!regexec(&section_expr, linebuf, SUBREGS, match_info, 0)) {
-      section = std::string(linebuf + match_info[1].rm_so,
-                            match_info[1].rm_eo - match_info[1].rm_so);
+      sections.push_back(
+        std::string(linebuf + match_info[1].rm_so,
+                    match_info[1].rm_eo - match_info[1].rm_so));
+
     } else if (!regexec(&value_expr, linebuf, SUBREGS, match_info, 0)) {
-      std::string key(linebuf + match_info[1].rm_so,
-                      match_info[1].rm_eo - match_info[1].rm_so);
-      std::string value(linebuf + match_info[2].rm_so,
-                        match_info[2].rm_eo - match_info[2].rm_so);
-      config[section][key] = value;
+      sections.back().add(
+        std::string(linebuf + match_info[1].rm_so,
+                    match_info[1].rm_eo - match_info[1].rm_so),
+        std::string(linebuf + match_info[2].rm_so,
+                    match_info[2].rm_eo - match_info[2].rm_so));
     }
   }
   cfgfile.close();
   regfree(&section_expr);
+  regfree(&value_expr);
 }
 
-const section_t & ConfigReader::section(std::string section) {
-  if (config.count(section))
-    return config[section];
-  throw "Config section not found.";
+
+std::list<ConfigReader::Section>::const_iterator ConfigReader::begin() const {
+  return sections.begin();
 }
 
-const std::string & ConfigReader::get(std::string section, std::string key) {
-  section_t s = this->section(section);
-  if (s.count(key))
-    return s[key];
-  throw "Config key not found.";
+std::list<ConfigReader::Section>::const_iterator ConfigReader::end() const {
+  return sections.end();
+}
+
+const ConfigReader::Section & ConfigReader::get(const std::string &name) const {
+  for (std::list<Section>::const_iterator s = sections.begin();
+       s != sections.end(); s++) {
+    if (s->name() == name)
+      return *s;
+  }
+  throw "ERROR: invalid section name.";
+}
+
+
+/////////////// ConfigReader::Section ////////////////
+
+ConfigReader::Section::Section(const std::string &name) :
+    _name(name), entries() { }
+
+void ConfigReader::Section::add(const std::string &k, const std::string &v) {
+  entries.insert(std::make_pair(k, v));
+}
+
+const std::string & ConfigReader::Section::get(const std::string &key) const {
+  std::map<std::string, std::string>::const_iterator e = entries.find(key);
+  if (e != entries.end())
+    return e->second;
+  throw "ERROR: invalid key name.";
+}
+
+const std::string & ConfigReader::Section::name() const {
+  return _name;
 }
 
 /* vim: set sw=2 sts=2 : */
--- ConfigReader.h
+++ ConfigReader.h
@@ -2,21 +2,33 @@
 #define _CONFIGREADER_
 
 #include <map>
+#include <list>
 #include <string>
-
-typedef std::map<std::string, std::string> section_t;
 
 class ConfigReader {
   public:
 
     ConfigReader(const std::string &path);
 
-    const section_t & section(std::string section);
-    const std::string & get(std::string section, std::string key);
+    class Section {
+      public:
+        Section(const std::string &name);
+        void add(const std::string &key, const std::string &value);
+        const std::string & get(const std::string &key) const;
+        const std::string & name() const;
+      protected:
+        std::string _name;
+        std::map<std::string, std::string> entries;
+    };
+
+    const Section & get(const std::string &name) const;
+
+    // section iterators
+    std::list<Section>::const_iterator begin() const;
+    std::list<Section>::const_iterator end() const;
 
   protected:
-
-    std::map<std::string, section_t> config;
+    std::list<Section> sections;
 };
 
 #endif /* _CONFIGREADER_ */
